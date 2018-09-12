d3629108c85aa4d66a7c32c090f0a188655a8aef
zt19994 <zt191610942@163.com>
2018-09-12 10:43:40 +0800

    OddEvenLinkedList

---
+++ src/Learn/linkedList/OddEvenLinkedList.java
@@ -0,0 +1,32 @@
+package Learn.linkedList;
+
+/**
+ * 将链表中奇数偶数分别组合排在一起，要按顺序
+ *
+ * @author zhongtao on 2018/9/12
+ */
+public class OddEvenLinkedList {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode oddEvenList(ListNode head) {
+        if (head != null) {
+            ListNode odd = head, even = head.next, evenHead = even;
+
+            while (even != null && even.next != null) {
+                odd.next = odd.next.next;
+                even.next = even.next.next;
+                odd = odd.next;
+                even = even.next;
+            }
+            odd.next = evenHead;
+        }
+        return head;
+    }
+}
