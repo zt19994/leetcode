200b1b3727bce86681076a4f77ef9c636d478c8b
zt19994 <zt191610942@163.com>
2018-09-26 08:42:43 +0800

    contains duplicate

---
+++ src/Learn/HashTable/ContainsDuplicate.java
@@ -0,0 +1,37 @@
+package Learn.HashTable;
+
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 包含重复
+ *
+ * @author zhongtao on 2018/9/26
+ */
+public class ContainsDuplicate {
+
+    /**
+     * 将nums加入set中，通过比较长度判断是否有重复的数字
+     */
+    public boolean containsDuplicate(int[] nums){
+        Set<Integer> hashSet = new HashSet<>();
+        int length = nums.length;
+        for (int num : nums) {
+            hashSet.add(num);
+        }
+        int size = hashSet.size();
+        return length != size;
+    }
+
+
+    /**
+     * 测试
+     */
+    @Test
+    public void test(){
+        int[] nums = {1, 2, 3, 1};
+        System.out.println(containsDuplicate(nums));
+    }
+}
