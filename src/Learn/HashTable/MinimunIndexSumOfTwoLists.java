7e5fb931b5ecad01f23afaf3e5162f6d0ee7d9a6
zt19994 <zt191610942@163.com>
2018-10-04 15:53:34 +0800

    minumun index sum of two lists

---
+++ src/Learn/HashTable/MinimunIndexSumOfTwoLists.java
@@ -0,0 +1,31 @@
+package Learn.HashTable;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 两链表最小索引和
+ *
+ * @author zhongtao on 2018/10/4
+ */
+public class MinimunIndexSumOfTwoLists {
+    public String[] findRestaurant(String[] list1, String[] list2) {
+        Map<String, Integer> map = new HashMap<>();
+        List<String> res = new LinkedList<>();
+        int minSum = Integer.MAX_VALUE;
+        for (int i = 0; i < list1.length; i++) map.put(list1[i], i);
+        for (int i = 0; i < list2.length; i++) {
+            Integer j = map.get(list2[i]);
+            if (j != null && i + j <= minSum) {
+                if (i + j < minSum) {
+                    res.clear();
+                    minSum = i + j;
+                }
+                res.add(list2[i]);
+            }
+        }
+        return res.toArray(new String[res.size()]);
+    }
+}
