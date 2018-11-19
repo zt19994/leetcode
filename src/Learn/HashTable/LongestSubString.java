fad1f8c28878d1d45a326bfdd5d08194615de3a1
zt19994 <zt191610942@163.com>
2018-11-19 09:41:25 +0800

    LongestSubString

---
+++ src/Learn/HashTable/LongestSubString.java
@@ -0,0 +1,57 @@
+package Learn.HashTable;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Given a string, find the length of the longest substring without repeating characters.
+ *
+ * @author zhongtao on 2018/11/19
+ */
+public class LongestSubString {
+
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0) {
+            return 0;
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        int max = 0;
+        for (int i=0, j=0; i<s.length(); i++){
+            if (map.containsKey(s.charAt(i))) {
+                j = Math.max(j, map.get(s.charAt(i)) + 1);
+            }
+            map.put(s.charAt(i), i);
+            max = Math.max(max, i - j + 1);
+        }
+        return max;
+    }
+
+    public int lengthOfLongestSubstring1(String s) {
+        int i = 0, j = 0, max = 0;
+        Set<Character> set = new HashSet<>();
+
+        while (j < s.length()) {
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j++));
+                max = Math.max(max, set.size());
+            } else {
+                set.remove(s.charAt(i++));
+            }
+        }
+
+        return max;
+    }
+
+    /**
+     * 测试
+     */
+    @Test
+    public void test(){
+        System.out.println(lengthOfLongestSubstring("abcabccdefgh"));
+        System.out.println(lengthOfLongestSubstring1("abcabccdefgh"));
+    }
+}
