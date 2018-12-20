760407ba124dc7694066e564acb24a5f01192ee5
zt19994 <zt191610942@163.com>
2018-12-20 16:33:45 +0800

    select sort

--- src/Learn/SortAlgorithm/SelectSort.java
+++ src/Learn/SortAlgorithm/SelectSort.java
@@ -2,7 +2,26 @@ package Learn.SortAlgorithm;
 
 /**
  * 选择排序
+ *
  * @author zhongtao on 2018/12/20
  */
 public class SelectSort {
+
+    public void selectSort(int[] arr) {
+        int minIndex = 0;
+        int temp;
+        for (int i = 0; i < arr.length - 1; i++) {
+            minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                // 找到当前循环最小值索引
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            temp = arr[i];
+            // 交换当前循环起点值和最小值索引位置的值
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
 }
